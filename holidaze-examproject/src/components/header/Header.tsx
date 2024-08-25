72ceb128dde6a9bd88bdb1cfe57ff9fb37782165
ArneBHuset <128044224+ArneBHuset@users.noreply.github.com>
2024-08-25 15:37:54 +0200

    added files, folder and begun om header component

---
+++ holidaze-examproject/src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Container } from '@mui/material';
+
+
+
+function Header() {
+  return (
+    <Container maxWidth={false} style={{background: "red"}}>
+
+    </Container>
+  );
+}
+
+export default Header;
