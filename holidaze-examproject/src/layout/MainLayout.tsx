ea7721efdc5afda046fd14c63e7afd242e48d7e4
ArneBHuset <128044224+ArneBHuset@users.noreply.github.com>
2024-08-28 11:16:57 +0200

    solved endless npm install bug by deleting @mui dependecies and reinstalling them to latest

--- holidaze-examproject/src/layout/MainLayout.tsx
+++ holidaze-examproject/src/layout/MainLayout.tsx
@@ -1,12 +1,31 @@
 import Header from '../components/header/Header.tsx';
-import { Container } from '@mui/material';
+import { Container, Box } from '@mui/material';
 
 function MainLayout() {
   return (
     <Container maxWidth={false} style={{ margin: 0, padding: 0 }}>
       <Header />
+      <Box component="main">{children}</Box>
     </Container>
   );
+}import React, { ReactNode } from "react";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+import { Box, Container } from "@mui/material";
+
+interface LayoutProps {
+  children: ReactNode;
 }
+
+const MainLayout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <Container maxWidth={false} disableGutters>
+      <Box>
+        <Header />
+        <Box component="main">{children}</Box>
+      </Box>
+    </Container>
+  );
+};
 
 export default MainLayout;
