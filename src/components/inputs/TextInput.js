010b8096ffcedf1b282295ed76f35ec6c51fdbc8
ArneBHuset <128044224+ArneBHuset@users.noreply.github.com>
2024-09-30 17:38:25 +0200

    resovled duplicate files issue

--- src/components/inputs/TextInput.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment } from 'react/jsx-runtime';
-import { TextField } from '@mui/material';
-function CustomInput() {
-  return _jsx(_Fragment, { children: _jsx(TextField, { fullWidth: true, label: 'Name' }) });
-}
-export default CustomInput;
